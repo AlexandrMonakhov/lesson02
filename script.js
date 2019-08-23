a5e1e0ad75885c89b69624764f2c6481e43c189c
Alexandr <31701404+AlexandrMonakhov@users.noreply.github.com>
2019-08-23 19:27:40 +0300

    Исправил доп. задание

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
-let num = 266219;
+let num = '266219';
 
+let mult = num[0] * num[1] * num[2] * num[3] * num[4] * num[5];
 // вывод переменной num
-console.log(num);
+console.log(mult);
 
 // возведение в степень
-let mult = num ** 3;
+let result = mult ** 3;
 
-console.log(String(mult).slice(0,2));
+console.log(String(mult).slice(0, 2));
