1fed7e0c8a45499a9a3b3022a91e9d57b75f0faf
Alexandr <31701404+AlexandrMonakhov@users.noreply.github.com>
2019-08-23 21:34:47 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,5 @@
-
 let num = 266219;
-let mul = 0;
 
-while (num > 0) {
-   mul *= num % 10;
-   num = Math.floor(num / 10);
-}
-
-// вывод переменной num
-console.log(mul);
-
-// // возведение в степень
-let result = mul ** 3;
-
-console.log(String(mul).slice(0, 2));
+ console.log((num.toString().split('').reduce((a, b) => a * b) ** 3).toString().slice(0, 2));
 
 
